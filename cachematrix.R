14b8bb5455865c5d3cccffc1b84a26bdde273c8e
ozgurgul <gul.ozgur@yahoo.com>
2015-01-25 19:08:12 +0000

    Message
    Changed

--- cachematrix.R
+++ cachematrix.R
@@ -28,7 +28,7 @@ cacheSolve <- function(x, ...) {
       
       m <- x$getInverse()
       if(!is.null(m)) {
-            message("getting cached matrix")
+            message("and the matrix has not changed")
             return(m)
       }
       
